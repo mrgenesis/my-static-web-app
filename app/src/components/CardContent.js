3f017b21324cc8e2203d0850b41448000219191d
Rosa, Genesis <mrgenesis@hotmail.com>
2020-09-16 14:40:13 -0400

    Configurado um novo campo price na view

--- app/src/components/CardContent.js
+++ app/src/components/CardContent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const CardContent = ({ name, description }) => (
+const CardContent = ({ name, description, price }) => (
   <div className="card-content">
     <div className="content">
       <div className="name">{name}</div>
       <div className="description">{description}</div>
+      <div className="name">R${price}</div>
     </div>
   </div>
 );
